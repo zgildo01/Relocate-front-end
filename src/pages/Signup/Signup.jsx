86f76457225bed9c9387fe6341f1fa522390fe11
smichaelonline <stephaniemichael001@gmail.com>
2022-10-27 00:06:58 -0400

    update sign up form with Mui

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import SignupForm from '../../components/SignupForm/SignupForm'
 import styles from './Signup.module.css'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
 
 const Signup = props => {
   const [message, setMessage] = useState([''])
@@ -10,11 +12,15 @@ const Signup = props => {
   }
 
   return (
-    <main className={styles.container}>
-      <h1>Sign Up</h1>
-      <p>{message}</p>
-      <SignupForm {...props} updateMessage={updateMessage} />
-    </main>
+    <div className={styles.container}>
+      <Box>
+        <Paper elevation={3}>
+          <h1>Sign Up</h1>
+          <p>{message}</p>
+          <SignupForm {...props} updateMessage={updateMessage} />
+        </Paper>
+      </Box>
+    </div>
   )
 }
 
