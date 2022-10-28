0e615138e7188a434c4e02b6c3e36b6c9802670f
smichaelonline <stephaniemichael001@gmail.com>
2022-10-27 23:53:33 -0400

    update new wishlist page

--- src/pages/WishLists/WishLists.jsx
+++ src/pages/WishLists/WishLists.jsx
@@ -1,14 +1,20 @@
 import styles from './WishLists.module.css'
 import WishlistCard from '../../components/WishlistCard/WishlistCard'
+import WishlistBanner from '../../assets/Wishlist-banner.png'
 
 const WishLists = (props) => {
 
   return (
-    <main className={styles.container}>
-      {props.wishlists?.map((wishlist) => (
-        <WishlistCard key={wishlist._id} wishlist={wishlist}/>
-      ))}
-    </main>
+    <>
+      <div className={styles.wishlistBanner}> 
+        <img src={WishlistBanner} alt="wishlist banner"/>
+      </div>
+      <main className={styles.container}>
+        {props.wishlists?.map((wishlist) => (
+          <WishlistCard key={wishlist._id} wishlist={wishlist}/>
+        ))}
+      </main>
+    </>
   )
 }
 
